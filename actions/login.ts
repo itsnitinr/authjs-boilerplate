fccb2658c215ff033accb9aca953cb04a4aab22b
Nitin Ranganath <nitinranganath@gmail.com>
2024-03-16 19:24:37 +0530

    prevent users from logging in without email verification

--- actions/login.ts
+++ actions/login.ts
@@ -1,11 +1,13 @@
 'use server';
 
 import * as z from 'zod';
+import { AuthError } from 'next-auth';
 
 import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
-import { AuthError } from 'next-auth';
+import { generateVerificationToken } from '@/lib/tokens';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -15,6 +17,17 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   }
 
   const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: 'Email does not exist' };
+  }
+
+  if (!existingUser.emailVerified) {
+    await generateVerificationToken(existingUser.email);
+    return { success: 'Verification email sent' };
+  }
 
   try {
     await signIn('credentials', {
--- auth.ts
+++ auth.ts
@@ -34,6 +34,19 @@ export const {
     },
   },
   callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== 'credentials') return true;
+
+      const existingUser = await getUserById(user.id as string);
+
+      // Prevent sign in without email verification
+      if (!existingUser?.emailVerified) {
+        return false;
+      }
+
+      return true;
+    },
     async jwt({ token }) {
       if (!token.sub) return token;
 
