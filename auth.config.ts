d06b4e5fd50960a1136257f68627ee0813f480ec
Nitin Ranganath <nitinranganath@gmail.com>
2024-03-16 18:27:03 +0530

    implement github and google oauth login

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,7 @@
 import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 import Credentials from 'next-auth/providers/credentials';
 
 import { LoginSchema } from '@/schemas';
@@ -7,6 +9,14 @@ import { getUserByEmail } from '@/data/user';
 
 export default {
   providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,16 +1,36 @@
 'use client';
 
+import { signIn } from 'next-auth/react';
 import { SiGoogle, SiGithub } from 'react-icons/si';
 
 import { Button } from '@/components/ui/button';
 
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+
 export const Social = () => {
+  const onClick = async (provider: 'google' | 'github') => {
+    signIn(provider),
+      {
+        callbackUrl: DEFAULT_LOGIN_REDIRECT,
+      };
+  };
+
   return (
     <div className="flex w-full gap-2 items-center">
-      <Button size="lg" variant="outline" className="w-full" onClick={() => {}}>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-full"
+        onClick={() => onClick('google')}
+      >
         <SiGoogle className="w-5 h-5" />
       </Button>
-      <Button size="lg" variant="outline" className="w-full" onClick={() => {}}>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-full"
+        onClick={() => onClick('github')}
+      >
         <SiGithub className="w-5 h-5" />
       </Button>
     </div>
