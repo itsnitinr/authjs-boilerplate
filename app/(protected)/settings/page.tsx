a9d52652ce130d420ecb4e9b1fc206e605ed4467
Nitin Ranganath <nitinranganath@gmail.com>
2024-03-16 17:03:37 +0530

    implement sign in and sign out with next auth

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,12 +1,20 @@
-import { auth } from '@/auth';
+import { auth, signOut } from '@/auth';
 
 const SettingsPage = async () => {
   const session = await auth();
 
   return (
     <div>
-      <h1>Settings</h1>
       {JSON.stringify(session, null, 2)}
+      <form
+        action={async () => {
+          'use server';
+
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+      </form>
     </div>
   );
 };
--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,35 @@
-import GitHub from 'next-auth/providers/github';
-
+import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+import { LoginSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+
+          // If there's no password, it means that the user used OAuth to sign in
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) {
+            return user;
+          }
+        }
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
