d81fd585975cb1aa47e50079fece9b1f269ed975
Nitin Ranganath <nitinranganath@gmail.com>
2024-03-16 16:26:35 +0530

    save registered user to database

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,6 +1,10 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
+
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 import { RegisterSchema } from '@/schemas';
 
@@ -10,6 +14,25 @@ export const signup = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: 'Invalid fields' };
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const userExists = await getUserByEmail(email);
+
+  if (userExists) {
+    return { error: 'Email already in use' };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO: Send verification email
 
   return { success: 'Email sent!' };
 };
