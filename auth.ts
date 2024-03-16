97ef7665f85ad734769fe1fcd486f0fcc96a9107
Nitin Ranganath <nitinranganath@gmail.com>
2024-03-16 18:31:15 +0530

    automatically verify email when user sign up using oauth

--- auth.ts
+++ auth.ts
@@ -20,6 +20,15 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  events: {
+    // Automatically verify email when a user signs up using OAuth
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
     async jwt({ token }) {
       if (!token.sub) return token;
